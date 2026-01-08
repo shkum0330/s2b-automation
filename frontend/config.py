41251bb5915517b93c14e8be5304a51ae2a1d843
shkum0330 <shkum0330@gmail.com>
2026-01-08 21:08:47 +0900

    update Jenkins와 GitHub Actions.md

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,3 @@
-# 개발 환경(로컬): "http://localhost:8080"
-# 배포 환경(운영): "http://your-server-ip.com" 또는 "https://api.your-domain.com"
+# BASE_URL = "http://localhost:8080"
 
-BASE_URL = "http://localhost:8080"
+BASE_URL = "https://reggie-superpositive-unstylishly.ngrok-free.dev/"
